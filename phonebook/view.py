e10c2e8b39aac7706a018299f753ae9367458651
Valeriy <cheremushkin.valeriy@mail.ru>
2023-01-25 22:42:16 +0400

    07

---
+++ phonebook/view.py
@@ -0,0 +1,31 @@
+
+
+
+def op():
+    op = int(input(
+        '1 - добавить пользователя \n 2 - вывести таблицу \n 3 - вывести только имя и фамилию \n 4 - отсортировать по имени \n 5 - отсортировать  по  id 6 - выход \n'))
+    return op
+
+def add_worker():
+    id = int(input('id = '))
+    name = input('name = ')
+    lastname = input('lastname = ')
+    number = int(input('number = '))
+    comments = input('comments = ')
+    line = f"{id},{name},{lastname},{number},{comments}\n"
+    with open("worker_list2.txt","a") as file:
+        file.write(line)
+    print("-----> сохранено")
+
+def print_table():
+    with open("worker_list2.txt","r") as file:
+        for line in file.readlines():
+            print(line, end = "")
+
+def print_only_names():
+    with open("worker_list2.txt","r") as file:
+        for line in file.readlines():
+            temp = line.split(",")
+            print(f"Имя - {temp[1]}, Фамилия - {temp[2]}")
+
+
